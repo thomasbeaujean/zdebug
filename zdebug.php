12397efe85c7dd0cdeecbc59d215590be9930688
thomasbeaujean <thomasbeaujean@users.noreply.github.com>
2016-02-02 12:23:44 +0100

    Update zdebug.php

--- zdebug.php
+++ zdebug.php
@@ -1,7 +1,26 @@
 <?php
 
-//include '/var/www/zdebug/zdebug.php';
+//include_once '/var/www/zdebug/zdebug.php';
+
 function zdebug($data)
 {
-	file_put_contents('/var/www/zdebug/log/log.txt',print_r($data,true),FILE_APPEND);
+    if ($data === '') {
+        $data = '[EMPTY STRING]';
+    }
+    if ($data === ' ') {
+        $data = '[ONE SPACE]';
+    }
+    if ($data === null) {
+        $data = '[NULL]';
+    }
+    if ($data === true) {
+        $data = '[true]';
+    }
+    if ($data === false) {
+        $data = '[false]';
+    }
+
+    $data = print_r($data, true);
+
+    file_put_contents('/var/www/zdebug/log/log.txt', $data."\n", FILE_APPEND);
 }
